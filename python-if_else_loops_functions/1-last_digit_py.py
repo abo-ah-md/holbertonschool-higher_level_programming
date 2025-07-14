4bd73dabd9865ed261ef4a73915e0829ce6e7e76
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-14 16:03:05 +0300

    added random number if statment using prin(f)

---
+++ python-if_else_loops_functions/1-last_digit_py.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    print(f"Last digit of {number} is -{abs(number) % 10} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {number % 10} and is " +
+        ("0" if number == 0 else "less than 6 and not 0"))
