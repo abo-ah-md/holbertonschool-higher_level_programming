091f92c14ec5a73a8b2c43c4ae37ceccb4b8a0c3
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-14 13:40:47 +0300

    fixed maybe !

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str= "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(word[39:67],word[107:111],word[0:6])
+str = str[39:67]+str[107:112]+str[0:6]
+print(str)
