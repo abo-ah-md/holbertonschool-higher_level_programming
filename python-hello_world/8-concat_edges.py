f132c9c5cac758500376290e3f866483c0d08918
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-14 13:22:58 +0300

    minpulating strings using string slicing with find

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,8 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:67]+str[107:111],str[0:6])
-
-
-#0:6 pyton
-#39 67 
-#107
