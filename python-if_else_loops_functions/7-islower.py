372b3a5f78666a96ddf4acb36f722b6349f98ede
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-17 22:47:21 +0300

    a

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) not in range(ord('a', ord('z') + 1)):
+    if ord(c) not in range(ord('a'), ord('z' + 1)):
         return False
     else:
         print(f"{c} is lower")
