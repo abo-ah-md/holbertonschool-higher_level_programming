29e9041accb7f374194c38993cb2049ee1af75b8
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-20 22:51:33 +0300

    added a script to remocve C and c from a string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if ord(my_string[i]) == ord("c"):
+    for i in range(len(my_string) -1):
+        if my_string[i] == "c":
                 my_string[i] = ""
-        if ord(my_string[i]) == ord("C"):
+        if my_string[i] == "C":
                 my_string[i] = ""
         
