87fadd30d3bb419200fa836c47e452fa650b734c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-22 16:07:17 +0300

    a

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a += tuple_a + (0,)
+    while len(tuple_b) < 2:
+        tuple_b += tuple_b +(0,)
+  
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+
+
+
