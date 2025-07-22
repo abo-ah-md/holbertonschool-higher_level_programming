d22ccbbd1bcd27b7314f219d00ee4aa66e5bae4b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-22 19:45:36 +0300

    a

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    max = my_list[0]
     if len(my_list) == 0:
         return None
     for i in my_list:
-        if i > max:
+        if i > max :
             max = i
     return max
