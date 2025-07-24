0935fc85b8785318b59b4b778fa9605d9ba82941
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-24 21:15:51 +0300

    a

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = None
         result = a/b
-    except(ValueError, ZeroDivisionError):
+    except(ZeroDivisionError):
         pass
     finally:
         print("Inside result: {}".format(result))
