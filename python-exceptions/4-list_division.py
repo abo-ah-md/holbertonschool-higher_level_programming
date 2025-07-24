b7c64fb2c8eabf3c81e3bb3409d21ea6be879a0e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-24 21:55:06 +0300

    a

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,10 +8,10 @@ def list_division(my_list_1, my_list_2, list_length):
             if isinstance(e, ZeroDivisionError):
                 print("division by 0")
                 new_list.append(0)
-            if isinstance(e, TypeError):
+            elif isinstance(e, TypeError):
                 print("wrong type")
                 new_list.append(0)
-            if isinstance(e, IndexError):
+            elif isinstance(e, IndexError):
                 print("out of range")
                 new_list.append(0)
         finally:
