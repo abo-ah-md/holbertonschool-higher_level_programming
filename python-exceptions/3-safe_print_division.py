4aed591829005c9a30b7294451319f67597ef6f0
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-24 21:13:37 +0300

    a

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result =None
-        result= a/b
+        result = None
+        result = a/b
     except:
         pass
     finally:
         print("Inside result: {}".format(result))
         return result
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
