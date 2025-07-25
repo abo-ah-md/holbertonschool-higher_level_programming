e23f5182a7916e6f36d3c92b02de011878946e51
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 21:59:43 +0300

    a

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this module use the function ay_my_name
+to return the complete name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ return the complete name """
+    if not isinstance(first_name,str) and not isinstance(last_name,str):
+        raise TypeError
+    elif not isinstance(first_name,str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name,str):
+        raise TypeError("last_name must be a string")
+    
+    return first_name + " " + last_name
