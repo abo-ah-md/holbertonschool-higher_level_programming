d434a4514b37f8fbb2b53d2933e4cd562126538d
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 22:04:50 +0300

    a

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,11 +7,10 @@ to return the complete name
 
 def say_my_name(first_name, last_name=""):
     """ return the complete name """
-    if not isinstance(first_name,str) and not isinstance(last_name,str):
+    if not isinstance(first_name, str) and not isinstance(last_name, str):
         raise TypeError
-    elif not isinstance(first_name,str):
+    elif not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name,str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
     print(f"My name is {first_name} {last_name}")
