b09714680327419ffc4604ea76ff3995887e6e4b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 19:21:18 +0300

    i really enjoid this test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" 
-this module use the function add_integer to add two integers and return the total
 """
+this module use the function add_integer
+to add two integers and return the total
+"""
+
+
 def add_integer(a, b=98):
     """this function will add two integers """
-    if not isinstance(a,(int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if  not isinstance(b,(int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if  a == float("inf") or a == float("-inf") :
+    if a == float("inf") or a == float("-inf"):
         raise OverflowError()
-    if  b == float("inf") or b == float("-inf") :
+    if b == float("inf") or b == float("-inf"):
         raise OverflowError()
     return int(a + b)
