16d41bcb0c4ab627a187bb2f320dea360eacf178
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-26 01:05:52 +0300

    a

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,7 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+class TestAddIntegerMethod(unittest.TestCase):
+    def test_position(slef):
+        slef.assertEqual(max_integer([1,5,6]),6)
+        slef.assertEqual(max_integer([8,5,6]),8)
+        slef.assertEqual(max_integer([1,11,2]),11)
