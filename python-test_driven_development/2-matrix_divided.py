2cfd0f3f180a937f55755a6ecdfe4396744c2e9d
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 20:37:40 +0300

    a

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this module use the function matrix_divided
+to divide a matrix  and return the new matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """divide a matrix  and return the new matrix """
+
+    size_of_list = 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(div,(int, float)):
+        raise TypeError("div must be a number")
+    
+    for i in range(len(matrix)):
+        if i == 0:
+            size_of_list = len(matrix[i])
+        if size_of_list != len(matrix[i]):
+            raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(matrix[i],list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for j in matrix[i]:
+            if not isinstance(j,(int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    return list(map(lambda row: list(map(lambda x:x / div),row),matrix))
