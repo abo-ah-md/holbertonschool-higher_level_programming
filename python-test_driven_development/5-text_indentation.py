c6861f1aa19164a3f03c9642a345aa2296a4dce5
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 23:50:43 +0300

    a

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,14 +9,18 @@ of these characters: ., ? and :
 def text_indentation(text):
     """print string with 2lines after ., ? and :"""
     n_line_char = [".", "?", ":"]
+    has_space = False
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in range(len(text)):
+        if has_space:
+            has_space = False
+            continue
         if text[i] in n_line_char:
             
             print(f"{text[i]}", end="\n\n")
-            if text[i + 1] == " ":
-                i += i
-                continue
+            if i+1 < len(text):
+                if text[i + 1].isspace():
+                    has_space = True
         else:
             print(f"{text[i]}", end="")
