06074663203cb41fc96b691e6e3ddac96d0e26e1
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-26 01:08:44 +0300

    a

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,6 @@ class TestAddIntegerMethod(unittest.TestCase):
         slef.assertEqual(max_integer([1,5,6]),6)
         slef.assertEqual(max_integer([8,5,6]),8)
         slef.assertEqual(max_integer([1,11,2]),11)
+        slef.assertEqual(max_integer([-1, -11,-2]),-1)
+        slef.assertEqual(max_integer([-5]),-5)
+        slef.assertEqual(max_integer([]),None)
