47a2dff9f374a13bb644f9c01d87988bb38fc8d1
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-25 19:13:29 +0300

    a

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,4 +4,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if  not isinstance(b,(int, float)):
         raise TypeError("b must be an integer")
+    if  a == float("inf") or a == float("-inf") :
+        raise OverflowError()
+    if  b == float("inf") or b == float("-inf") :
+        raise OverflowError()
     return int(a + b)
