941b870ac7ef3655eeeb17221da6646a44bcbd0e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-27 08:13:05 +0300

    a

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,4 +6,17 @@ This module is for creating a square class
 
 
 class Square():
+    """
+    A minimal class representing a square.
+    
+    This class currently has no attributes or methods.
+    It's essentially a placeholder or a base class for future use.
+    """
     pass
+
+    
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
