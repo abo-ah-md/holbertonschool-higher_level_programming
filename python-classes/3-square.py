28d06079c43e96bd51b13842b0e31ada9a28c8e7
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-27 15:29:26 +0300

    a

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,7 +19,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """this is an init dor the square class """
         return self.__size ** 2
-    
