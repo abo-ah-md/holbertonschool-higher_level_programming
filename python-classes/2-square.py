2f41e44e7dd480b6a0cc30a7b720e215cdd479b0
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-27 14:50:09 +0300

    a

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+This module is for creating a square class
+'''
+
+
+class Square:
+    """
+    A minimal class representing a square.
+    This class currently has 1 private instanse attribute
+    called size and no methods.
+    It's essentially a placeholder or a base class for future use.
+    """
+    def __init__(self, size=0):
+        """this is an init dor the square class """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
