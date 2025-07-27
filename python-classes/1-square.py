d233cad3c3a59928919d7c7f38f0bb6390021739
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-27 08:29:38 +0300

    a

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This module is for creating a square class
+'''
+
+
+class Square:
+    """
+    A minimal class representing a square.
+    This class currently has 1 private instanse attribute
+    called size and no methods.
+    It's essentially a placeholder or a base class for future use.
+    """
+    def __init__(self, size):
+        self.__size = size
