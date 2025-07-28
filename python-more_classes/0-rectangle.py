c807d28379031b2513d508d6811972d443947951
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-28 15:49:27 +0300

    added python module that has an empty rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+This module is for creating a rectangle class
+'''
+
+
+class Rectangle:
+    """
+    A minimal class representing a Rectangle.
+    This class currently
+    """
+    def __init__(self):
+        """this is an init for the Rectangle class """
+        self
