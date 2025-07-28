0ad7dd317e95c0067768138d55110e8e402ff253
abo-ah-md <abo-ah-md1@hotmail.com>
2025-07-28 23:28:51 +0300

    added a rectangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,10 +23,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """this is an setter for the Rectangle class """
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -37,8 +37,9 @@ class Rectangle:
     @height.setter
     def height(selfl, value):
         """this is an setter for the Rectangle class """
-        if value < 0:
-            raise ValueError("height must be >= 0")
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         selfl.__height = value
