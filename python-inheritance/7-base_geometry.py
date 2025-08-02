163dd05884c3b266f35cdcd25d418e4dd7d80eba
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 02:17:45 +0300

    a

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,8 +16,19 @@ class BaseGeometry:
         """
         pass
 
+    def integer_validator(self, name, value):
+
+        """
+        this  for valdating the value 
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
     def area(self):
         """
         this module for creating an empty class
         """
         raise Exception("area() is not implemented")
+    
--- python-inheritance/main.py
+++ python-inheritance/main.py
@@ -3,20 +3,20 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
+bg.ia("my_int", 12)
+bg.ia("width", 89)
 
 try:
-    bg.integer_validator("name", "John")
+    bg.ia("name", "John")
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    bg.integer_validator("age", 0)
+    bg.ia("age", 0)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    bg.integer_validator("distance", -4)
+    bg.ia("distance", -4)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
