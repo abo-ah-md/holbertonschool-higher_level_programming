87dc4b8b04dcb5124447f677f59cadc3952c6332
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 00:26:58 +0300

    made a function that returns all object attributes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+This function is for returning all object attributers 
+'''
+
+
+def lookup(obj):
+    '''
+    This function is for returning all object attributers 
+    '''
+
+    return dir(obj)
