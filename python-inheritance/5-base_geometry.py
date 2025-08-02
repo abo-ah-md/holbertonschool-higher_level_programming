dd2861889313749cc50edc50dd3ef3a0a55255ad
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 01:44:50 +0300

    a

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+this module for creating an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    this module for creating an empty class
+    """
+
+    def __init__(self):
+        """
+        this module for creating an empty class
+        """
+        pass
