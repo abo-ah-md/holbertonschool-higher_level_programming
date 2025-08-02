0a9675b860e6ffaf367c8be01bc5617711343939
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 01:32:53 +0300

    wrote a function is for returning if obect is an exact instanse of calss

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This function is for returning if obect is an exact instanse of calss
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    This function is for returning if obect is an exact instanse of calss
+    """
+
+    return isinstance(obj, a_class)
