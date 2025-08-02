deb939a6c60ba966690a18d0164d26aaefe622c2
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 01:48:00 +0300

    a

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+this module for creating an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    this module for creating an empty class
+    """
+
+    def __init__(self):
+        """
+        this module for creating an empty class
+        """
+        pass
+
+    def area(self):
+        """
+        this module for creating an empty class
+        """
+        raise Exception("area() is not implemented")
