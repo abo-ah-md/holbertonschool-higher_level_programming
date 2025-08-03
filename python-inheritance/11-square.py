fa06d77e32485294520b3ab8130015beff148b7b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 13:06:10 +0300

    a

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+this module for creating a class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square is a sub class of Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Square is a sub class of Rectangle
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        calculating area of Square
+        """
+        return self.__size ** 2
+    
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
+    
+    def __repr__(self):
+        print(f"[Square] {self.__size}/{self.__size}")
+    
