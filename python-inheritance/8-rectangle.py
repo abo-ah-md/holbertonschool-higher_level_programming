0e26dcd9b8526d5f41735945ca6aa0768b72065d
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-03 03:06:21 +0300

    a

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,7 +3,8 @@
 """
 this module for creating a class
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """
