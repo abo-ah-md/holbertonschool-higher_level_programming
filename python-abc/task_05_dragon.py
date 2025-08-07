8ecb0e1536fce60a06b54d7bdd1c2413e97f8658
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-07 14:38:49 +0300

    a

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -4,6 +4,7 @@
 This module creates a Dragon class using mixins for added behavior.
 """
 
+
 class SwimMixin:
     """
     This mixin adds swimming behavior.
@@ -11,12 +12,14 @@ class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
+
 class FlyMixin:
     """
     This mixin adds flying behavior.
     """
     def fly(self):
         print("The creature flies!")
+
 
 class Dragon(SwimMixin, FlyMixin):
     """
