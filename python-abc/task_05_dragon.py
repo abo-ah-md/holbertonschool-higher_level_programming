a1fee06736e94f17993e42b555909907c71cef96
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-07 14:57:54 +0300

    a

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -32,6 +32,19 @@ class Dragon(SwimMixin, FlyMixin):
     """
     A Dragon that can swim, fly, and roar.
     """
+    def __init__(self,ability=""):
+        """
+        This mixin adds flying behavior.
+        """
+        self.ability = ability
+
+    def abt(self):
+        """
+        This mixin adds flying behavior.
+        """
+        print(f"The creature {self.ability}s")
+    
+
     def roar(self):
         """
         This mixin adds flying behavior.
@@ -39,7 +52,8 @@ class Dragon(SwimMixin, FlyMixin):
         print("The creature roars!")
 
 if __name__ == "__main__":
-    draco = Dragon()
+    draco = Dragon("drive a clude")
     draco.swim()
     draco.fly()
     draco.roar()
+    draco.abt()
