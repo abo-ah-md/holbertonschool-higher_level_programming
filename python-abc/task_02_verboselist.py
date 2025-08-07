91b0fc87a2a44c48247b55ecdce03ff3bc08a320
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-07 09:03:00 +0300

    a

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+this module for creating an abc subclass
+"""
+class VerboseList(list):
+    """
+    this calss extends some of the methods in list with printed message
+    """
+
+
+    def extend(self,iterable):
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items.")
+
+    def append(self, elem):
+        super().append(elem)
+        print(f"Added [{elem}] to the list.")
+
+    def remove(self,elem):
+        print(f"Removed [{elem}] from the list.")
+        super().remove(elem)
+
+    def pop(self,elem = -1):
+        print(f"Popped [{self[elem]}] from the list.")        
+        super().pop(elem)
+        
