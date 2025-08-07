dff24ddc9c6bd998758ad1465973071697925135
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-07 13:23:03 +0300

    a

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+this module for creating an abc subclass
+"""
+class CountedIterator():
+    """
+    this calss extends some of the methods in list with printed message
+    """
+
+    def __init__(self, data):
+        self.iterator = iter(data)
+        self.counter = 0
+
+    def __next__(self):
+        self.counter += 1
+        if self.iterator[self.counter] is False:
+            raise StopIteration()
+        else:
+            return next(self.iterator)
+        
+    def get_count(self):
+        return self.counter
