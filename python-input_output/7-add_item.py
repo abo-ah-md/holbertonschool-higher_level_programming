843c93d5fe31b680464d86186054ea864d72ba17
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 01:34:15 +0300

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,15 @@
 """This function is for craeting an object from a json file"""
 import sys
 import os
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 if __name__ == "__main__":
-    filename= "add_item.json"
+    filename = "add_item.json"
     if os.path.exists(filename):
         load_from_json_file(filename)
     else:
         arg_list = []
         arg_list.extend(sys.argv[1:])
-        save_to_json_file(arg_list,filename)
-    
+        save_to_json_file(arg_list, filename)
