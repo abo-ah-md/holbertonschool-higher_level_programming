a55d526ae672feb0d82a9d31f6088bbb6623eabd
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 01:16:13 +0300

    a

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """This function is for craeting an object from a json file"""
 
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
