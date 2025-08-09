5c6b5dac9eaf769a51af6cf4728841f0f3259bc5
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 01:14:58 +0300

    a

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This function is for craeting an object from a json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function is for craeting an object from a json file"""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.load(file)
