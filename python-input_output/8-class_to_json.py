e6f5b0093b74beee849bfd1eb1fd9a61ad60d80b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 02:18:18 +0300

    a

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This function is for craeting json from class dict"""
 
-import json
+
 def class_to_json(obj):
-    return json.dumps(__dict__(obj))
+    return __dict__(obj)
