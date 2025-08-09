9c1294aac5f34e79551d1e3d695ebc04365ca7c7
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 00:28:18 +0300

    a

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,9 +7,9 @@ This function is for Reading a file
 
 def read_file(filename=""):
     """
-This function is for Reading a file
+    This function is for Reading a file
     """
 
     with open(filename) as file:
         data = file.read()
-        print(data,end="")
+        print(data, end="")
