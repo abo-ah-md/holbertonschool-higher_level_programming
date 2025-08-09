8735e9d7388d83e52213b67015937127ee3d74d2
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 02:09:36 +0300

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,6 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 if __name__ == "__main__":
     filename = "add_item.json"
-    try:
-        arg_list = load_from_json_file(filename)
-    except FileNotFoundError as e:
-        arg_list = []
     arg_list = load_from_json_file(filename)
     arg_list.extend(sys.argv[1:])
     save_to_json_file(arg_list, filename)
