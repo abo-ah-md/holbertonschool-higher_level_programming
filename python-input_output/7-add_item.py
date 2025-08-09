c744acfc0b706c612a9f9c65fae0fec2d04e9696
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 02:13:35 +0300

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """This function is for craeting an object from a json file"""
 import sys
 import os
+
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
