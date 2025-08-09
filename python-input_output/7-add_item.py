465237e19323feaa4d31a061a8436134a274585c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 01:41:17 +0300

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,10 +8,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 if __name__ == "__main__":
     filename = "add_item.json"
-    if os.path.exists(filename):
-        load_from_json_file(filename)
-    else:
-        arg_list = []
-
+    arg_list = []
     arg_list.extend(sys.argv[1:])
     save_to_json_file(arg_list, filename)
+    load_from_json_file(filename)
