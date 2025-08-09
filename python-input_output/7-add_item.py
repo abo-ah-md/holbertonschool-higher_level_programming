1dc2e88b36aac8eb097ed1f2f83eb8cde111514e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-10 02:10:56 +0300

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 if __name__ == "__main__":
     filename = "add_item.json"
-    arg_list = load_from_json_file(filename)
+    arg_list = []
     arg_list.extend(sys.argv[1:])
     save_to_json_file(arg_list, filename)
     load_from_json_file(filename)
