88a167c543186404f6ab0f01289ce85d7cb046a6
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-16 19:35:36 +0300

    a

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -4,6 +4,33 @@
 
 import xml.etree.ElementTree as ET
 
+
+def datatype_checker(value):
+
+    if value is None:
+        return None
+
+
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+
+
+    try:
+        return int(value)
+    except ValueError:
+        pass
+
+
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+
+    return value
+
 def serialize_to_xml(dictionary, filename):
     try:
 
@@ -21,6 +48,8 @@ def deserialize_from_xml(filename):
         tree = ET.parse(filename)
         root = tree.getroot()
         data = {}
+        
         for child in root :
-            data[child.tag] = child.text
+            correct_data_type = datatype_checker(child.text)
+            data[child.tag] = correct_data_type
         return data
