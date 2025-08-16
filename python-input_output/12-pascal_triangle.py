8305edaf0a04699fe6292d5ab3636c11bd16a114
abo-ah-md <abo-ah-md1@hotmail.com>
2025-08-16 19:46:31 +0300

    a

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Generates Pascal's Triangle as a list of lists of integers.
+"""
+
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal's triangle of n rows."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]] 
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        row = [1]
+        
+        for j in range(1, i):
+            row.append(prev_row[j-1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
