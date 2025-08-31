3be0620f183b0d8e7fa1ad37f4c267d08b8b682b
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 02:10:20 +0300

    a

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- this to create a full table in  the db in the server
+CREATE TABLE IF NOT EXISTS second_table(
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) DEFAULT NULL,
+score INT NOT NULL,
+PRIMARY KEY (id)
+);
+
+INSERT INTO second_table (name,score) 
+values
+('John',10),
+('Alex',3),
+('Bob',14),
+('George',8);
+
