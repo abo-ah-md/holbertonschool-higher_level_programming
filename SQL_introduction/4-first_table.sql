4e09851c1811af1e885b08ba98f854accaac4808
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 01:03:12 +0300

    a

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 -- this to create a table in  the db in the server
 CREATE TABLE IF NOT EXISTS first_table(
-id INT,
-name VARCHAR(256)
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) DEFAULT NULL,
+PRIMARY KEY (id)
 );
