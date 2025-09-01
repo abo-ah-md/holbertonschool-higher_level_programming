b9de4ffb808860d3f3a3e858d4466354f94982ef
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 06:13:18 +0300

    a

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,10 @@
 -- this is sub querry using forign key
 SELECT 
-    tv_shows.title,
-    tv_genres.name
+    tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres 
+JOIN tv_show_genres 
     ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres 
+JOIN tv_genres 
     ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
-
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
