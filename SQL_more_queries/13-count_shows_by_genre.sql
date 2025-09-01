53c025c46fdd2222477493dd790c8009554bf56f
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 06:00:42 +0300

    a

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- this is sub querry using forign key
 SELECT 
     tv_genres.name AS genre,
-    COUNT(tv_show_genres.show_id)
+    COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
-JOIN  genre
-    ON tv_genres = tv_show_genres.genre_id
-    GROUP BY  tv_genres.name
-ORDER BY genres.name ASC;
+JOIN tv_genres 
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
