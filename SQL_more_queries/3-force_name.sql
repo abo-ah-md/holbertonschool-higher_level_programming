62681b2034651599ca34ea0deae18961de076529
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 03:56:45 +0300

    A

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,2 @@
 -- this is for listing all users prevlages
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL DEFAULT 'Unknown',
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256) NOT NULL); 
