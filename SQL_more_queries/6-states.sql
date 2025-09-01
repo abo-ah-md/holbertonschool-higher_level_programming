babcd45300d91615b788b3c31fb3680a05d1084c
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 04:06:45 +0300

    A

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- this is for listing all users prevlages
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_2;
+CREATE TABLE IF NOT EXISTS states (
+     id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
