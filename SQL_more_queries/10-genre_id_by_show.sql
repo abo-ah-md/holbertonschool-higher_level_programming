e7743a59a89ee181ebf2ce222f2b93c203963114
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 05:33:33 +0300

    a

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- this is sub querry using forign key
 SELECT 
-    tv_shows.title AS title,
-    tv_show_genres.genre_id AS genre_id
+    tv_shows.title,
+    tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres 
     ON tv_shows.id = tv_show_genres.show_id
-ORDER BY genre_id, title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
