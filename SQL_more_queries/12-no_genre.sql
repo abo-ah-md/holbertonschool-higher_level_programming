5630cfefa87f4eb88f1208007bbaf5887226412a
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 05:56:39 +0300

    a

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,9 @@
 -- this is sub querry using forign key
 SELECT 
-    tv_genres.name AS genre,
-    COUNT(tv_show_genres.show_id)
-FROM tv_show_genres
-JOIN  genre
-    ON tv_genres = tv_show_genres.genre_id
-    GROUP BY  tv_genres.name
-ORDER BY genres.name ASC;
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres 
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
