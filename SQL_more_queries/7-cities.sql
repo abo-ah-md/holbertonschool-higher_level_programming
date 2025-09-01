fa50b089f8074041d119dbe4623098c544927606
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 04:13:04 +0300

    A

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- this is for listing all users prevlages
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    FOREIGN KEY (state_id) REFERENCES states(id) INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
