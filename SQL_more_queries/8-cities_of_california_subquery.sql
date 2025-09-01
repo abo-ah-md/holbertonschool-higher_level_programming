ca23a928ede29adafe6c093ec4f3431b9c7f01c8
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 04:59:25 +0300

    a

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,9 @@
 -- this is sub querry using forign key
-SELECT id,name from cities WHERE state_id = (
-SELECT id FROM states WHERE name = 'California')
+SELECT id, name
+FROM cities
+WHERE state_id IN (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
 ORDER BY id ASC;
