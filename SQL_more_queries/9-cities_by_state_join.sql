2da540dbfc0b6d23b091e19659563b3a1414ab48
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-01 05:15:41 +0300

    a

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,9 @@
 -- this is sub querry using forign key
-SELECT cities.id, cities.name, states.name FROM cities 
-JOIN states ON cities.state.id = state_id
-ORDER BY ASC;
+SELECT 
+    cities.id AS id,
+    cities.name AS name,
+    states.name AS name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
 
