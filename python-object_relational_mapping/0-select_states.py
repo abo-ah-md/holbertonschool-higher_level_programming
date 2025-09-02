064e0ec74d4bf85f984344111e9464a23c4691ca
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-03 01:47:30 +0300

    added mysqldb script to fetch all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd={sys.argv[2]},
-        db={sys.argv[3]}
+        passwd=sys.argv[2],
+        db=sys.argv[3]
         )
     
     cursor = conn.cursor()
