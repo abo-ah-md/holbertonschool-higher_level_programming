e00e103ed523c625a1bec4be985f8b5c195d3b02
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-04 16:00:49 +0300

    a

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM cities ORDER BY id ASC"
+        "SELECT * FROM cities ORDER BY cities.id ASC"
     )
     for row in cursor.fetchall():
         print(row)
