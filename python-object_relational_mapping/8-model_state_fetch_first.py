36cc7104b3215d303ed7d90704c058258221efc6
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-04 17:39:03 +0300

    a

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,6 +15,9 @@ if __name__ == "__main__":
     )
     session = Session(engine)
     states = session.query(State).order_by(State.id).first()
-    print(f"{states.id}: {states.name}")
+    if states:
+        print(f"{states.id}: {states.name}")
+    else:
+        print("Nothing\n")
 
     session.close()
