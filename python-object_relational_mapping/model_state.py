7fb1d96c8f81f629315ac1ea04493f66ef7a1f31
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-04 17:14:27 +0300

    a

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-Safe from SQL injections.
+Contains the class definition of a State and
+an instance Base = declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    State class that links to the MySQL table 'states'
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,autoincrement=True,nullable=False)
-    name =  Column(String(128),nullable=False)
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
