24012197c9b9dbe9b89c793b6e4bdd79bbea0632
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-04 14:59:01 +0300

    a

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,13 +7,17 @@ import sys
 import MySQLdb
 
 
-def list_all_states():
+def main():
     """
     Lists all states from the database hbtn_0e_0_usa sorted by id.
     Usage: ./0-select_states.py <mysql username> <mysql password>
     <database name>
     """
     try:
+
+        if len(sys.argv) != 5:
+            print("Usage: ./3-my_safe_filter_states.py <username> <password> <database> <state_name>")
+            return
         user1 = sys.argv[1]
         pass1 = sys.argv[2]
         db1 = sys.argv[3]
@@ -24,13 +28,15 @@ def list_all_states():
             port=3306, host="localhost", user=user1, passwd=pass1, db=db1
         )
         c = database.cursor()
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s\
+        query = "SELECT * FROM states WHERE name = %s\
                 ORDER BY states.id ASC"
         c.execute(query, (match,))
         [print(state) for state in c.fetchall()]
     except MySQLdb.Error as e:
         print(f"Error connecting to databse, {e}")
     finally:
+        if len(sys.argv) != 5:
+            return
         if c:
             c.close()
         if database:
@@ -38,4 +44,4 @@ def list_all_states():
 
 
 if __name__ == "__main__":
-    list_all_states()
+    main()
