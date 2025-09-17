6344f09d72dd3d37d2a63b423ed904f58055489e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-17 05:51:30 +0300

    a

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-function add(a, b){
-  if (Number(a) && Number(b)) {
-console.log(`${Number(a) + Number(b)}`);
-} 
+function add (a, b) {
+  if (Number(a) || Number(b)) {
+    console.log(`${Number(a) + Number(b)}`);
+  }
 }
 
-add(argv[2],argv[3]);
-
-
+add(argv[2], argv[3]);
