97c3786535e985d0ee6eab968d2ccd4e54440df5
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-17 06:31:56 +0300

    a

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv[2] === 1) {
-  console.log(NaN);
-  
+if (!argv[2]) {
+  console.log(1);
 }
 function recursiveFactorial (num) {
   if (num === 1) {
