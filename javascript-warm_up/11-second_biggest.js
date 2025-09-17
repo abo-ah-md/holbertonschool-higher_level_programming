c0d8711afbe04eb1e08f66d31e69a73fb4b6d089
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-17 06:46:47 +0300

    a

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (!argv[2] || !argv[3] || Number(argv[2]) === 1) {
+  console.log(0);
+} else {
+  const arr = [];
+  for (let i = 2; i < argv.length; i++) {
+    arr.push(argv[i]);
+  }
+  arr.sort((a, b) => b - a);
+  console.log(arr[1]);
+}
