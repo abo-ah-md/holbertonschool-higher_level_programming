68f41c73a998defacc2d3b42ab6b994f863b754e
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-22 14:27:17 +0300

    added scripts to fetch data

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,3 +1,36 @@
+const data = await getdata();
+const newCharList= createAlistFromObject(data);
+document.querySelector('#character').appendChild(newCharList);
+
+
+
+function createAlistFromObject(obj){
+    const ol= document.createElement('ol');
+    for (const key in obj){
+        if(obj.hasOwnProperty(key)){
+            const li =document.createElement('li');
+            const value= obj[key];
+            if(typeof value == 'object' && value != null){
+                li.textContent=`${key}:`;
+                li.appendChild(createAlistFromObject(
+                Array.isArray(value)?
+                value.reduce((acc,cur,i)=> ({...acc,[i]:cur}),{})
+                :value
+            ));
+        }
+            else{
+                li.textContent=`${key}:${value}`;
+            }
+        ol.appendChild(li);
+
+        }
+    }
+    return ol;
+}
+
+
+
+
 async function getdata() {
 const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json'
 try{
@@ -6,7 +39,7 @@ if (!response.ok) {
     throw new Error(`response status : ${response.status}`);
 }
 const result = await response.json();
-console.log(result)
+return result;
 }
  catch(error){
     console.log(error.message)
