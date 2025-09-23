1df99e83eca5102f9e9441504e0c5d0699c2e383
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-23 05:04:10 +0300

    a

---
+++ javascript-dom_manipulation/100-script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('.my_list');
+
+  function addList () {
+    const addBtn = document.getElementById('add_item');
+    addBtn.addEventListener('click', () => {
+      const li = document.createElement('li');
+      li.innerText = 'Item';
+      list.appendChild(li);
+    });
+  }
+
+  function removeList () {
+    const removeBtn = document.querySelector('#remove_item');
+    removeBtn.addEventListener('click', () => {
+      if (list.lastElementChild) {
+        list.lastElementChild.remove();
+      }
+    });
+  }
+
+  function clearList () {
+    const clearBtn = document.querySelector('#clear_list');
+    clearBtn.addEventListener('click', () => {
+      list.innerHTML = '';
+    });
+  }
+
+  addList();
+  removeList();
+  clearList();
+});
