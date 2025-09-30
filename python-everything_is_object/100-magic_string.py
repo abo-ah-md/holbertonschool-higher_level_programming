51538c09a8023413746627bd1110a558fcd9b9d3
abo-ah-md <abo-ah-md1@hotmail.com>
2025-09-30 15:18:26 +0300

    a

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(count=[0]):
-    count[0] +=1
-    return ', '.join(['BestSchool']) * count[0]
+def magic_string(_count=[0]):
+    _count[0] +=1
+    return ', '.join(['BestSchool']) * _count[0]
